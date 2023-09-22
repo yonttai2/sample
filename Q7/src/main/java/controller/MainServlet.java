8b0bb785d92a360d951520774cb05d0a77c8a61e
yongtae <aosldk1346@gmail.com>
2023-09-22 17:29:40 +0900

    hotfix commit;

--- Q7/src/main/java/controller/MainServlet.java
+++ Q7/src/main/java/controller/MainServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class TestSerlvet
+ * Servlet implementation class MainServlet
  */
-@WebServlet("/TestSerlvet")
-public class TestSerlvet extends HttpServlet {
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-     
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestSerlvet() {
+    public MainServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
