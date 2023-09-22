352616745e6f154428ea0f72eb4d62345b567eb7
yongtae <aosldk1346@gmail.com>
2023-09-22 17:18:34 +0900

    master commit

--- Q7/src/main/java/controller/TestSerlvet.java
+++ Q7/src/main/java/controller/TestSerlvet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestSerlvet")
 public class TestSerlvet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
     /**
      * @see HttpServlet#HttpServlet()
      */
